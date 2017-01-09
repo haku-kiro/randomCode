09148b3b8d26f425f279a17436af2f04619de0a9
haku-kiro <botPrime@yahoo.com>
2017-01-09 19:01:07 +0200

    adding files

---
+++ pythonSpiral/pySpiral.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+t = Turtle()
+
+t.speed(0)
+def spiral(n):
+    if n < 1000:
+        t.forward(n)
+        t.right(91)
+        spiral(n+2)
+spiral(2)
+input("Press any key to continue....")
---
+++ whatsAppSpamBot/spamWhatsapp.py
@@ -0,0 +1,15 @@
+#selenium works on websites, so to use this script you have to sign into whatsapp web (QR code)
+#you may have to install selenium as well:
+#pip install selenium
+
+from selenium import webdriver
+b = webdriver.Firefox() #have to check browser compatibility
+b.get('https://web.whatsapp.com') #https ?
+input()
+elem = b.find_element_by_xpath('//span[contains(text(), "mat")]') 
+elem.click()
+elemTextbox = b.find_elements_by_class_name('input')
+i = 0
+while i < 5:
+    elemTextbox[1].send_keys('test')
+    b.find_element_by_class_name('send-container').click()
