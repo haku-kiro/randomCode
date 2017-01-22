6e9fe20fbe4190a772e20d967ffe63ef62c2c14a
haku-kiro <botPrime@yahoo.com>
2017-01-22 21:23:50 +0200

    Adding the pyBot files

---
+++ pyBasicTwitterBot/getImg.sh
@@ -0,0 +1,5 @@
+#Super simple bash command, wget, to download the image:
+
+wget http://loremflickr.com/1024/512/space,random,abstract -O img.jpg
+#The -O switch allows you to name a file that is downloaded.
+#The site I downloaded a random image from is loremflickr, visit them for more information on how their site works. It basically is a static URL that changes to random images.
